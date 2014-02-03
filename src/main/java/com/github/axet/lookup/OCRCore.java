f38f69472339c35b446d7cd5bed55ecef8266505
Alexey Kuznetsov <axet@me.com>
2014-02-03 20:39:10 +0400

    public

--- src/main/java/com/github/axet/lookup/OCRCore.java
+++ src/main/java/com/github/axet/lookup/OCRCore.java
@@ -85,7 +85,7 @@ public class OCRCore {
         detector.setGaussianKernelRadius(1f);
     }
 
-    List<FontSymbol> getSymbols() {
+    public List<FontSymbol> getSymbols() {
         List<FontSymbol> list = new ArrayList<FontSymbol>();
 
         for (FontFamily f : fontFamily.values()) {
@@ -95,7 +95,7 @@ public class OCRCore {
         return list;
     }
 
-    List<FontSymbol> getSymbols(String fontFamily) {
+    public List<FontSymbol> getSymbols(String fontFamily) {
         return this.fontFamily.get(fontFamily);
     }
 
